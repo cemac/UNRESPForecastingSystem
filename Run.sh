7d04718837414c9eef69793622f2dedc5e2a725e
James O'Neill <earjjo@leeds.ac.uk>
2018-05-02 10:41:09 +0100

    Avoid deleting placeholder README in TERREL output directory

--- Run.sh
+++ Run.sh
@@ -9,8 +9,11 @@ cd CALPUFF_EXE
 ifort -O0 -fltconsistency ../CALPUFF_SRC/TERREL/terrel.for -o terrel_intel.exe
 cd ..
 #Remove any old files:
-rm -rf ./CALPUFF_OUT/TERREL/*
 rm -rf *.dat *.grd *.lst *.sav
+cd CALPUFF_OUT/TERREL
+shopt -s extglob
+rm -- !(README)
+cd ../..
 #Run TERREL:
 ./CALPUFF_EXE/terrel_intel.exe ./CALPUFF_INP/terrel.inp
 #Move output files:
