4da78a6ba262017b0ac1533d2b968784f0408372
James O'Neill <earjjo@leeds.ac.uk>
2018-05-02 10:57:04 +0100

    Only compile TERREL if exe doesn't exist; make run script more verbose

--- Run.sh
+++ Run.sh
@@ -4,17 +4,29 @@
 module load intel/17.0.0
 
 ###TERREL###
-#Compile TERREL:
+#Compile TERREL if required:
 cd CALPUFF_EXE
-ifort -O0 -fltconsistency ../CALPUFF_SRC/TERREL/terrel.for -o terrel_intel.exe
+if [ ! -f ./terrel_intel.exe ]; then
+    echo "### COMPILING TERREL"
+    ifort -O0 -fltconsistency ../CALPUFF_SRC/TERREL/terrel.for -o terrel_intel.exe 
+    echo " ---> FINISHED ###"
+else
+    echo "### TERREL ALREADY COMPILED ###"
+fi
 cd ..
-#Remove any old files:
+#Remove any old files before running:
+echo -n "### DELETING OLD TERREL OUTPUT FILES"
 rm -rf *.dat *.grd *.lst *.sav
 cd CALPUFF_OUT/TERREL
 shopt -s extglob
 rm -- !(README)
 cd ../..
+echo " ---> FINISHED ###"
 #Run TERREL:
-./CALPUFF_EXE/terrel_intel.exe ./CALPUFF_INP/terrel.inp
+echo "### RUNNING TERREL"
+./CALPUFF_EXE/terrel_intel.exe ./CALPUFF_INP/terrel.inp > terrel.log
+echo " ---> FINISHED ###"
 #Move output files:
+echo -n "### MOVING TERREL OUTPUT FILES"
 mv *.dat *.grd *.lst *.sav ./CALPUFF_OUT/TERREL/.
+echo " ---> FINISHED ###"
