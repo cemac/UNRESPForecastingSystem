1108c06a42b625c1d13846e75a6ac3c8efa3bb80
hlburns <H.L.Burns@leeds.ac.uk>
2018-11-05 15:14:34 +0000

    feat: bash commands
    Will link files to public html

--- Run.sh
+++ Run.sh
@@ -326,5 +326,17 @@ if [ "$runVIS" = true ]; then
   cd ../..
   echo " ---> FINISHED ###"
 fi
-
+cwd=$(pwd)
+FNAME=$(date +%Y%m%d)
+VIZPATH=$'~/public_html/UNRESP/UNRESP_VIZ/'
+cd vis/$FNAME
+mogrify -format jpg *.png
+if [ ! -e $VIZPATH$FNAME ]
+then
+  mkdir $VIZPATH$FNAME
+fi
+mv *.jpg $VIZPATH$FNAME
+cd $VIZPATH
+ln -sf $VIZPATH$FNAME/*.jpg .
+cd $cwd
 echo "### SUCCESSFULLY COMPLETED FORECAST ###"
