af5d8d86b9030b26439647e8537298e9e7a954ef
hlburns <H.L.Burns@leeds.ac.uk>
2018-12-04 15:44:35 +0000

    chore: updated shell scripts
    including test google maps

--- updatesite.sh
+++ updatesite.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
 cwd=$(pwd)
 FNAME=$(date +%Y%m%d)
-VIZPATH=$'~/public_html/UNRESP/UNRESP_VIZ/'
+VIZPATH=~/public_html/UNRESP/UNRESP_VIZ/
 cd vis/$FNAME
 mogrify -format jpg *.png
+setfacl -m other:r-x *
+chmod og+rx *
 if [ ! -e $VIZPATH$FNAME ]
 then
   mkdir $VIZPATH$FNAME
 fi
-mv *.jpg $VIZPATH$FNAME
+mv *.jpg *.html $VIZPATH$FNAME
 cd $VIZPATH
 ln -sf $VIZPATH$FNAME/*.jpg .
+ln -sf $VIZPATH$FNAME/*.html .
 cd $cwd
