367500367a3d467716b16e4d22a6ac887da47b9b
hlburns <H.L.Burns@leeds.ac.uk>
2018-12-05 18:15:47 +0000

    feat: google maps
      Google maps page added,
      Js and html adjusted accordingly
      API key added the genmaps.py
      git ignore to remove key from version control

--- updatesite.sh
+++ updatesite.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+rundate=$(date +%Y%m%d)
+# If the vis hasn't been done yet
+runVIS=false
+if [ "$runVIS" = true ]; then
+  echo "### RUNNING VISUALISATION TOOLS"
+  rm -rf ./vis/${rundate}
+  mkdir ./vis/${rundate}
+  cd Python
+  ./generateMaps.py ${rundate}
+  cd ..
+  cd vis/${rundate}
+  ffmpeg -f image2 -r 4 -i static_concrec0100%02d.png -vcodec mpeg4 -y -s 7680x4320 movie_${rundate}.mp4
+  cd ../..
+  echo " ---> FINISHED ###"
+fi
 cwd=$(pwd)
 FNAME=$(date +%Y%m%d)
 VIZPATH=~/public_html/UNRESP/UNRESP_VIZ/
 cd vis/$FNAME
-mogrify -format jpg *.png
+# mogrify -format jpg *.png
 setfacl -m other:r-x *
 chmod og+rx *
 if [ ! -e $VIZPATH$FNAME ]
