1a6acda369981bc6aefeeb5ea045df3c47027918
hlburns <H.L.Burns@leeds.ac.uk>
2019-02-19 17:17:05 +0000

    :hammer_and_wrench: command line script to plot from map toolkit

--- Python/genmaps.py
+++ Python/genmaps.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 """genmaps
 .. module:: genmaps
     :platform: Unix
@@ -17,22 +19,131 @@ Example:
 .. CEMAC_UNRESPForcastingSystem:
    https://github.com/cemac/UNRESPForcastingSystem
 """
+from __future__ import print_function
+import argparse
 from dateutil.parser import parse
 import maptoolkit as mtk
-# Flags
+
+# Manual Flags (will be overwrittend by commandline flas)
 StaticMaps = True
 GoogleMaps = True
+SO24 = True
 # Not yet Functioning
 diffMaps = False
 windfield = False
 Layers = False
-# Standalone Functions
 # READ IN COMMAND LINE ARGUMENTS
-parser = argparse.ArgumentParser(description="Used to generate a series (48hrs) of static and interactive (google) maps \
-         showing SO2 concentrations around the Masaya volcano, as predicted by the CALPUFF dispersion model")
-parser.add_argument("date", help="Date string, format YYYYMMDD, of the current CALPUFF run. Used to locate \
-                    directory containing the SO2 output files (with assumed naming convention 'concrec0100**.dat', \
-                    where '**' goes from '01' through to '48'", type=str)
+dstring = ("Used to generate a series (48hrs) of static and interactive" +
+           "(google) maps \n showing SO2 concentrations around the Masaya " +
+           "volcano, as predicted by the CALPUFF dispersion model")
+hstring = ("Date string, format YYYYMMDD, of the current CALPUFF run. Used " +
+           "to + locate \n  directory containing the SO2 output files (with " +
+           "assumed naming convention 'concrec0100**.dat', \n where '**' " +
+           " goes from '01' through to '48'")
+parser = argparse.ArgumentParser(description=dstring)
+parser.add_argument("date", help=hstring, type=str)
+# Switches
+parser.add_argument('--all', help='Plots all types of maps',
+                    action='store_true')
+parser.add_argument('--layers', help='Plots as raster layers',
+                    action='store_true')
+parser.add_argument('--custom', help='Allows single switches',
+                    action='store_true')
+parser.add_argument('--SO24', help=r'Plot both SO$_2$ and SO$_4$',
+                    action='store_true')
+parser.add_argument('--SO2', help=r'Plot SO$_2$ only',
+                    action='store_true')
+parser.add_argument('--SO4', help=r'Plot SO$_4$ only',
+                    action='store_true')
+parser.add_argument('--static', help='Turn on static',
+                    action='store_true')
+parser.add_argument('--google', help='Turn on googlemaps',
+                    action='store_true')
+parser.add_argument('--diff', help='Turn on IMO vs UoL',
+                    action='store_true')
+parser.add_argument('--wind', help='Turn on wind quiverplots (NAM)',
+                    action='store_true')
 args = parser.parse_args()
+
+if args.all:
+    SO24 = True
+    StaticMaps = True
+    GoogleMaps = True
+    diffMaps = True
+    windfield = True
+    Layers = False
+    print('some options selected are not yet available')
+    diffMaps = False
+    windfield = False
+
+if args.all and args.custom:
+    print('--all and --custom can not be used together, setting to custom')
+
+if args.layers:
+    Layers = True
+    print('some options selected are not yet available')
+    Layers = False
+
+# Bespoke options
+if args.custom:
+    SO24 = False
+    StaticMaps = False
+    GoogleMaps = False
+    diffMaps = False
+    windfield = False
+    Layers = False
+
+if args.static:
+    StaticMaps = True
+if args.google:
+    GoogleMaps = True
+if args.diff:
+    diffMaps = True
+    print('some options selected are not yet available')
+    diffMaps = False
+if args.wind:
+    windfield = True
+    print('some options selected are not yet available')
+    windfield = False
+# echo what is being done
+print('Generating Maps with following settings:')
+print("StaticMaps = ", StaticMaps)
+print("GooogleMaps =", GoogleMaps)
+print(r"Both SO$_2$ and SO$_4$ = ", SO24)
+print("IMO vs UoL =", diffMaps)
+print("NAM quiver plots", windfield)
+print("Plot as raster layers", Layers)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 date = args.date
 mpt = mtk(date)
+
+if StaticMaps:
+    if SO24:
+        mpt.plot_staticmaps('topo', SOX=r'SO$_2$')
+        mpt.plot_staticmaps('topo', SOX=r'SO$_4$')
+        mpt.plot_staticmaps('satellite', SOX=r'SO$_2$')
+        mpt.plot_staticmaps('satellite', SOX=r'SO$_4$')
+    elif SO2:
+        mpt.plot_staticmaps('topo', SOX=r'SO$_2$')
+        mpt.plot_staticmaps('satellite', SOX=r'SO$_2$')
+    elif SO4:
+        mpt.plot_staticmaps('topo', SOX=r'SO$_4$')
+        mpt.plot_staticmaps('satellite', SOX=r'SO$_4$')
+    else:
+        print(r'Conc must be set to SO$_2$ or SO$_4$ or both (default)')
+        print(r'most likely the --custom flag has been used with out:')
+        print(' --SO24', ' --SO2', 'or  --SO4',)
+if GoogleMaps:
+    if SO24:
+        mpt.plot_googlemaps(SOX=r'SO$_2$')
+        mpt.plot_googlemaps(SOX=r'SO$_4$')
+    elif SO2:
+        mpt.plot_googlemaps(SOX=r'SO$_2$')
+    elif SO4:
+        mpt.plot_googlemaps(SOX=r'SO$_4$')
+    else:
+        print(r'Conc must be set to SO$_2$ or SO$_4$ or both (default)')
+        print(r'most likely the --custom flag has been used with out:')
+        print(' --SO24', ' --SO2', 'or  --SO4',)
