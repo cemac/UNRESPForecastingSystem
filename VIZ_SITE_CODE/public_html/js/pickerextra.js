bfab2f82f4cca4178784eeee767661844efe3b44
hlburns <H.L.Burns@leeds.ac.uk>
2019-03-28 17:08:09 +0000

    :pencil2: minor tweaks

--- VIZ_SITE_CODE/public_html/js/pickerextra.js
+++ VIZ_SITE_CODE/public_html/js/pickerextra.js
@@ -59,7 +59,7 @@ window.onload = function() {
     var name = target.name;
     var value = type === 'checkbox' ? target.checked : target.value;
     var relatedElement;
-
+    pickerContainer.style.display = 'block';
     switch (name) {
       case 'container':
         if (value) {
@@ -120,74 +120,12 @@ window.onload = function() {
     }
   });
 
-
-  // Examples
-  // --------------------------------------------------
-
-  new Picker(document.querySelector('.js-date-picker'), {
-    format: 'MMM D, YYYY',
-    text: {
-      title: 'Pick a date',
-    },
-  });
-
-  new Picker(document.querySelector('.js-time-picker'), {
-    format: 'HH:mm',
-    headers: true,
-    text: {
-      title: 'Pick a time',
-    },
-  });
-
-  new Picker(document.querySelector('.js-full-picker'), {
-    controls: true,
-    format: 'YYYY-MM-DD HH:mm:ss.SSS',
-    headers: true,
-  });
-
-  new Picker(document.querySelector('.js-month-picker'), {
-    format: 'MMMM',
-    text: {
-      title: 'Pick a month',
-    },
-  });
-
-  new Picker(document.querySelector('.js-inline-picker'), {
-    controls: true,
-    inline: true,
-  });
-
-  new Picker(document.querySelector('.js-mini-picker'), {
-    container: '.js-mini-picker-container',
-    inline: true,
-    rows: 1,
-  });
-
-  new Picker(document.querySelector('.js-super-picker'), {
-    container: '.js-super-picker-container',
-    format: 'YYYY年M月D日H时m分',
-    increment: {
-      minute: 10,
-    },
-    text: {
-      title: '选择日期时间',
-      cancel: '取消',
-      confirm: '确认',
-    },
-    translate(type, text) {
-      const suffixes = {
-        year: '年',
-        month: '月',
-        day: '日',
-        hour: '时',
-        minute: '分',
-      };
-
-      return Number(text) + suffixes[type];
-    },
-  });
+ 
 
   if (typeof hljs !== 'undefined') {
     hljs.initHighlightingOnLoad();
   }
+  if (result) {
+  return result
+}
 };
