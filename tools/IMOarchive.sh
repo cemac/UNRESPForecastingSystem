06d39a46f32f1fbfce32bc46168e2a1eebc827fe
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-04 17:57:47 +0100

    :bulb: adding headers

---
+++ tools/IMOarchive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -
+#title          :generic_archiving.sh
+#description    :Archive tool
+#author         :CEMAC - Helen
+#date           :20190404
+#version        :0.1-beta
+#usage          :./generic_archiving.sh
+#notes          :
+#bash_version   :4.2.46(2)-release
+#============================================================================
