ebd465cba5b01f6387f413f0442da464fcf6bdf3
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-05 15:19:18 +0100

    :sparkles: archiving tool phase 1 #12

--- tools/CALPUFFarchive.sh
+++ tools/CALPUFFarchive.sh
@@ -8,32 +8,17 @@
 #notes          :
 #bash_version   :4.2.46(2)-release
 #============================================================================
-# Set current and archive location
-current=~earunres/public_html/UNRESP_VIZ/
-#archive="/ds/shared/Earth&Environment/Research/SEE/Research-1/UNRESP/Image_current/"
-archive="/ds/shared/Earth&Environment/Research/SEE/Research-1/UNRESP/UNRESPForecastingSystem/VIZ_SITE_CODE/public_html/UNRESP_VIZ"
-# Set year to archive
-year=2019
-# Create the year folder in the archive space
-cd $archive
-if [ ! -e  $year ]
-then
-  mkdir $year
-fi
-# Create a folder for each month
-cd $year
-for i in $(seq -f "%02g" 1 12)
-  do
-    mkdir m$year$i
-  done
-cd $current/
-for d in $year*/; do
-    md=${d:(-5)}
-    m=${md:(0):(2)}
-    folder=$archive/$year/m$year$m/$d
-    if [ ! -e $folder ]
-    then
-      mkdir $folder
-    fi
-    cp -p $d* $folder
-done
+
+# Archive CALPUFF_OUT
+echo "Archiving CALPUFF OUTPUT"
+in=~earmgr/CEMAC/UNRESPForecastingSystem/CALPUFF_OUT/CALPUFF/
+out="/ds/shared/Earth&Environment/Research/SEE/Research-1/UNRESP/UNRESPForecastingSystem/CALPUFF_OUT/CALPUFF/"
+./generic_archiving.sh -i $in -o $out -b
+echo "Archived CALPUFF OUTPUT"
+
+# Archive CALMET
+echo "Archiving CALMET OUTPUT"
+in=~earmgr/CEMAC/UNRESPForecastingSystem/CALPUFF_OUT/CALMET/
+out="/ds/shared/Earth&Environment/Research/SEE/Research-1/UNRESP/UNRESPForecastingSystem/CALPUFF_OUT/CALMET/"
+./generic_archiving.sh -i $in -o $out -b
+echo "Archived CALMET OUTPUT"
