9bea77891b421be298c741a3513babee70e813bb
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-05 15:19:18 +0100

    :sparkles: archiving tool phase 1 #12

--- tools/generic_archiving.sh
+++ tools/generic_archiving.sh
@@ -8,6 +8,7 @@
 #notes          :
 #bash_version   :4.2.46(2)-release
 #============================================================================
+
 print_usage() {
   echo "
  generic_archiving.sh
@@ -17,15 +18,36 @@ print_usage() {
   .\generic_archiving.sh <opts>
 
  Options:
-  -d <directory> directory
-  -f <file> filename or pattern
+  -d <date> YYYYMM
+  -i <starting loaction>
+  -o <output location>
+  -b sets bulk archiving
+  -n set
  "
 }
-
-while getopts 'd:f:h' flag; do
+# Set current and archive location defaults
+in=~earmgr/CEMAC/UNRESPForecastingSystem/NAM_data/raw/
+out="/ds/shared/Earth&Environment/Research/SEE/Research-1/UNRESP/UNRESPForecastingSystem/NAM_data"
+year=$(date +%Y)
+# find previous month
+m=$(date --date="$(date +%Y%m15) -1 month" +%Y%m)
+bulk=true
+set_bulk() {
+  bulk=true
+}
+# A flag for NAM processed
+nam=true
+set_nam() {
+  nam=true
+}
+# Set year to out
+while getopts 'd:i:o:bnh' flag; do
   case "${flag}" in
-    d) folder="${OPTARG}" ;;
-    f) fpat="${OPTARG}" ;;
+    d) date="${OPTARG}" ;;
+    i) in="${OPTARG}" ;;
+    o) out="${OPTARG}" ;;
+    n) set_nam ;;
+    b) set_bulk ;;
     h) print_usage
        exit 1 ;;
     *) print_usage
@@ -33,10 +55,65 @@ while getopts 'd:f:h' flag; do
   esac
 done
 
-
-if [ ! -e $folder ]
-then
-  mkdir $folder
+if [ "$bulk" = false ]; then
+  echo "Archiving month"
+  year=${m:(-5)}
+  # Check and create the year folder in the archive space
+  cd $out
+  if [ ! -e  $year ]
+  then
+    mkdir $year
+  fi
+  cd $year
+  # Check and create the year month folders in the archive space
+  if [ ! -e  m$year$i ]
+  then
+    mkdir m$year$mm
+  fi
+  cd $in/
 fi
 
-mv $fpat $folder
+if [ "$bulk" = true ]; then
+  echo "Archiving current year"
+  # Check and create the year folder in the archive space
+  cd $out
+  if [ ! -e  $year ]
+  then
+    mkdir $year
+  fi
+  cd $year
+  # Check and create the year month folders in the archive space
+  for i in $(seq -f "%02g" 1 12)
+    do
+      if [ ! -e  m$year$i ]
+      then
+        mkdir m$year$i
+      fi
+    done
+  cd $in/
+  if [ "$nam" = true ]; then
+    for d in *$year*; do
+      # extract month
+      m=${d:(8):(2)}
+      folder=$out/$year/m$year$m/
+      if [ ! -e $folder ]
+      then
+        mkdir $folder
+      fi
+      cp -p $d* $folder
+    done
+  else
+    for d in $year*/; do
+      md=${d:(-5)}
+      m=${md:(0):(2)}
+      folder=$out/$year/m$year$m/$d
+      if [ ! -e $folder ]
+      then
+        mkdir $folder
+      fi
+      cp -p $d* $folder
+    done
+  fi
+  echo "data copied to " $out
+  echo "run checks and delete duplicates from " $in
+fi
