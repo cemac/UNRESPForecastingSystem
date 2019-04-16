126c6453c363b2c6484af8a678eddace13c2f427
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-16 14:49:36 +0100

    :globe_with_meridians: add in support for included files and google
    * work with iframes
    * remove errors from unloaded elements by not executing on incomplete 
    loaded page

--- VIZ_SITE_CODE/public_html/js/animecontrols.js
+++ VIZ_SITE_CODE/public_html/js/animecontrols.js
@@ -1,15 +1,17 @@
 var animation = document.getElementById("animation")
 var nextslideindex = 0
 var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel"
-if (animation.attachEvent)
-  animation.attachEvent("on" + mousewheelevt, rotateimage)
-else if (animation.addEventListener)
-  animation.addEventListener(mousewheelevt, rotateimage, false, passive = true)
-if (animation.attachEvent)
-  animation.attachEvent('onclick', clickflip)
-else if (animation.addEventListener)
-  animation.addEventListener('click', clickflip, false)
-if (window.attachEvent)
-  window.attachEvent('keydown', keyflip)
-else if (window.addEventListener) //WC3 browsers
-  window.addEventListener('keydown', keyflip, false)
+if (typeof animation !== 'undefined' && animation !== null) {
+  if (animation.attachEvent)
+    animation.attachEvent("on" + mousewheelevt, rotateimage)
+  else if (animation.addEventListener)
+    animation.addEventListener(mousewheelevt, rotateimage, false, passive = true)
+  if (animation.attachEvent)
+    animation.attachEvent('onclick', clickflip)
+  else if (animation.addEventListener)
+    animation.addEventListener('click', clickflip, false)
+  if (window.attachEvent)
+    window.attachEvent('keydown', keyflip)
+  else if (window.addEventListener) //WC3 browsers
+    window.addEventListener('keydown', keyflip, false)
+}
