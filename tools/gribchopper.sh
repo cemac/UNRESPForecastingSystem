13657258aaa517788b3e7ecdb1f9754aca5ad3f3
hlburns <H.L.Burns@leeds.ac.uk>
2019-06-18 15:23:44 +0100

    :pencil2: :beetle: filename mismatch

--- tools/gribchopper.sh
+++ tools/gribchopper.sh
@@ -12,7 +12,7 @@
 
 # NAM FILES contain 712 Vars accross the whole of central america
 # We want just 139 for a specific region
-yearmnt=201703
+yearmnt=201712
 location="/nfs/earcemac/projects/unresp/nam_data"
 cd $location
 mkdir ${yearmnt}_small
@@ -29,15 +29,15 @@ for line in $d ; do
     echo "shrinking to region " $f
     wgrib2 $f -small_grib 272.096000:277.928000 10.073999:15.905999 small.grb
     echo "removing unwated variables"
-    wgrib2 small.grb2 -s | egrep '(:PRMSL:|:HGT:|:TMP:|:RH:|:DZDT:|:UGRD:|:VGRD:)' | wgrib2 -i small.grb2 -grib tiny.grb
-    rm -f small.grb
+    wgrib2 small.grb -s | egrep '(:PRMSL:|:HGT:|:TMP:|:RH:|:DZDT:|:UGRD:|:VGRD:)' | wgrib2 -i small.grb -grib tiny.grb
     echo "removing unwanted pressure levels"
     wgrib2 tiny.grb -s | egrep -e '(:1000 mb:|:100 mb:|:150 mb:|:1000 mb:|:10 mb:|:200 mb:|:20 mb:|:250 mb:|:2 mb:|:300 mb:|:30 mb:|:400 mb:|:500 mb:|:50 mb:|:5 mb:|:600 mb:|:700 mb:|:75 mb:|:7 mb:|:800 mb:|:850 mb:|:900 mb:|:925 mb:|:950 mb:|:PRMSL:)' | wgrib2 -i tiny.grb  -grib min.grb
-    rm -f tiny.grb
     rm -f $f
+    rm -f tiny.grb
+    rm -f small.grb
     mv min.grb $f
   done
   cd ..
 done
 echo "complete.."
-echo "check "
+echo "check and delete"
