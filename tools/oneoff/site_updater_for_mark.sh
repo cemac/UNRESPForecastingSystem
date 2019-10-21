164e8ece52aff289b59e3fa7240f04fe63c022e6
helen <h.l.burns@leeds.ac.uk>
2019-10-21 12:44:37 +0100

    :sparkles: tool to modify site written for mark

---
+++ tools/oneoff/site_updater_for_mark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -
+#title          :site_updater_for_mark.sh
+#description    :Implement changes to unresp site
+#author         :CEMAC - Helen
+#date           :20191021
+#version        :1.0
+#usage          :./site_updater_for_mark.sh
+#notes          :
+#bash_version   :4.2.46(2)-release
+#============================================================================
+
+newcodehome='/nfs/earcemac/projects/unresp/ForecastVisualized/UNRESPForecastingSystem/VIZ_SITE_CODE/public_html/'
+oldcodehome='~earunres/public_html'
+
+cp ${newcodehome}/_includes/* ${oldcodehome}/_includes/
+cp ${newcodehome}/css/custom4.css ${oldcodehome}/css/custom4.css
+cp ${newcodehome}/css/s*.css ${oldcodehome}/css/
+cp ${newcodehome}/js/anime.js ${oldcodehome}/js/
+cp ${newcodehome}/UNRESP_VIZ/*html ${oldcodehome}/UNRESP_VIZ/
+cp  ${newcodehome}/UNRESP_VIZ/AQSensor/*html ${oldcodehome}/UNRESP_VIZ/AQSensor/
+
+cd $oldcodehome
+
+chmod -R ogu+rX *
