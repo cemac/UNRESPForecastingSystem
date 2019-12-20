505a5ebba93d6c5796074c691a368156d5027ab5
helen <h.l.burns@leeds.ac.uk>
2019-12-20 15:14:18 +0000

    :hammer_and_wrench: plotting of scatter clocks etc added

--- Analysis/Stats.py
+++ Analysis/Stats.py
@@ -37,6 +37,9 @@ if BACKEND == 'Qt4Agg' and sys.version_info[0] == 2:
     mpl.pyplot.switch_backend('Agg')
 
 Towns = ['ElPanama', 'Pacaya']
+NormalisePlots = False
+CompositePlots = False
+ScatterPlots = False
 
 
 def dateparse(x): return pd.datetime.strptime(x, '%d/%m/%Y %H:%M')
@@ -47,7 +50,7 @@ def dateparse(x): return pd.datetime.strptime(x, '%d/%m/%Y %H:%M')
 # ------------------------ 1. Normalise ------------------------------------ #
 
 
-def gen_normalised_plots(town):
+def gen_normalised_plots(town, plot=None):
     """gen_normalised_plots
     ..description: generate normalised plots from data sets, rescale to maxium
                    = 1?
@@ -80,27 +83,103 @@ def gen_normalised_plots(town):
     # Pop back into a dataframe
     df = pd.DataFrame(x_scaled, columns=['Observations', 'NAM', 'ECMWF'],
                       index=All.index)
-    # Plot
-    fig, ax = plt.subplots(figsize=(10, 5))
-    df.plot(ax=ax)
-    plt.title('Normalised Observation, NAM, ECMWF for ' + town +
-              '\n (model data = area average)')
-    plt.ylabel('Normalised (via min/max scaler) concentration)')
-    plt.xlabel('Date in March 2017 (hourly data)')
-    plt.savefig('Timeseries_Normalised.png')
-    return df
+    if plot:
+        # Plot
+        fig, ax = plt.subplots(figsize=(10, 5))
+        df.plot(ax=ax)
+        plt.title('Normalised Observation, NAM, ECMWF for ' + town +
+                  '\n (model data = area average)')
+        plt.ylabel('Normalised (via min/max scaler) concentration)')
+        plt.xlabel('Date in March 2017 (hourly data)')
+        plt.savefig(town + '_Timeseries_Normalised.png')
+    return df, All
 
 # Notes ElPanama
 # ECMWF Looked Crazy at the end of March and we had no data for end of March
+# so i removed that data?
 
 
-plt.clf()
-for town in Towns:
-    df = gen_normalised_plots(town)
-    profile = pandas_profiling.ProfileReport(df)
-    profile.to_file(town + 'normalised_stats.html')
+if NormalisePlots is True:
+    for town in Towns:
+        df = gen_normalised_plots(town, plot='Y')[0]
+        plt.clf()
+        profile = pandas_profiling.ProfileReport(df)
+        profile.to_file(town + 'normalised_stats.html')
 
-# ------------------------ 2. Dayly maxium (normailised)-------------------- #
+# ------------------------ 2. Composite days (normalised)-------------------- #
+if CompositePlots is True:
+    for town in Towns:
+        for i in np.arange(2):
+            fig, ax = plt.subplots(figsize=(10, 5))
+            df = gen_normalised_plots(town)[i]
+            df = df.groupby(df.index.hour).mean()
+            if i == 1:
+                x = df.values
+                # scikit learn makes this quick
+                min_max_scaler = preprocessing.MinMaxScaler()
+                x_scaled = min_max_scaler.fit_transform(x)
+                # Pop back into a dataframe
+                df = pd.DataFrame(x_scaled, columns=['Observations', 'NAM', 'ECMWF'],
+                                  index=df.index)
+            df.plot(ax=ax)
+            plt.title('Composite mean hourly concentrations (Normalised)')
+            plt.ylabel('Normalised concentration')
+            plt.xlabel(' Hour in day')
+            plt.savefig(town + 'Composite_day' + str(i) + '.png')
+            plt.clf()
 
+if ScatterPlots is True:
+    for town in Towns:
+        df = gen_normalised_plots(town)[0]
+        # add an hour of day column
+        df['hour'] = df.index.hour
+        # Plot as scatter plot
+        fig, ax = plt.subplots(figsize=(10, 5))
+        plt.scatter(df.hour, df.Observations, marker='d', alpha=0.5)
+        plt.scatter(df.hour, df.NAM, marker='*', alpha=0.6)
+        plt.scatter(df.hour, df.ECMWF, marker='x', alpha=0.6)
+        plt.title('Scatter plot of Normalised concentrations grouped by hour' +
+                  ' of day \n' + town)
+        plt.ylabel('Normalised concentrations')
+        plt.xlabel('hour of day')
+        plt.legend(['Obs', 'NAM', 'ECMWF'])
+        plt.savefig(town+'scatterbyhour.png')
+        plt.clf()
+        # Plot as rose/ clock thing..
+        times = [6, 3, 0,  21, 18, 15, 12, 9]
+        fig = plt.figure(figsize=(12, 10))
+        ax = fig.add_subplot(221, projection='polar')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.Observations,
+                    alpha=0.60, marker='d', label='observations')
+        ax.set_xticklabels(times)
+        ax.set_title('\n Observations')
+        plt.legend(scatterpoints=5, loc="upper left", bbox_to_anchor=(1.04, 1))
+        ax = fig.add_subplot(222, projection='polar')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.NAM,
+                    alpha=0.80, marker='x', color='orange', label='NAM')
+        ax.set_xticklabels(times)
+        ax.set_title('NAM')
+        plt.legend(scatterpoints=5, loc="upper left", bbox_to_anchor=(1.04, 1))
+        ax = fig.add_subplot(223, projection='polar')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.ECMWF,
+                    alpha=0.60, marker='+', color='g', label='ECMWF')
+        ax.set_xticklabels(times)
+        ax.set_title('ECMWF')
+        plt.legend(scatterpoints=5, loc="upper left", bbox_to_anchor=(1.04, 1))
+        ax = fig.add_subplot(224, projection='polar')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.Observations,
+                    alpha=0.60, marker='d', label='observations')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.NAM,
+                    alpha=0.80, marker='x', label='NAM')
+        plt.scatter(np.pi / 2.0 - 2 * np.pi * df.hour / 24, df.ECMWF,
+                    alpha=0.60, marker='+', label='ECMWF')
+        ax.set_xticklabels(times)
+        ax.set_title('All')
+        plt.legend(scatterpoints=5, loc="upper left", bbox_to_anchor=(1.04, 1))
+        plt.suptitle('Scatter plots in polar co-ordinates of normalised ' +
+                     'concentrations \n for ' + town + '\n')
+        plt.tight_layout()
+        plt.savefig(town + 'normalised_clock_scatter.png')
+        plt.clf()
 
-# ------------------------ 3. Comosite days (normalised)-------------------- #
+# ------------------------ 3. Dayly maxium (normailised)-------------------- #
